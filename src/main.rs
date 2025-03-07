5cdd6b9a1cc4cec95aa70d4e1053d1b91da40772
a-korkin <a-korkin88@yandex.ru>
2025-03-07 08:24:09 +0300

    fix: read bytes to chars

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
 use std::{
-    io::BufRead,
-    io::{BufReader, Read, Write},
+    io::{Read, Write},
     net::{TcpListener, TcpStream},
-    str,
+    // str,
 };
 
 fn handler(mut stream: TcpStream) {
     let mut buf = [0; 128];
-    let n = stream.read(&mut buf).unwrap();
-    println!("bytes: {}\nmsg: {}", n, str::from_utf8(&buf).unwrap());
+
+    let mut iter: u8 = 0;
+    loop {
+        iter += 1;
+        let bytes = stream.read(&mut buf).unwrap();
+        // println!("{}", str::from_utf8(&buf).unwrap());
+        let chars = buf.iter().map(|a| *a as char).collect::<Vec<char>>();
+        println!("iter: {}\n{:?}", iter, chars);
+        if buf == "".as_bytes() {
+            println!("empty");
+        }
+        if buf == "\r\n".as_bytes() {
+            println!("check");
+        }
+        if bytes == 0 {
+            break;
+        }
+        buf = [0; 128];
+    }
+
     let response = "HTTP/1.1 200 OK\r\n";
     stream.write(response.as_bytes()).unwrap();
 }
