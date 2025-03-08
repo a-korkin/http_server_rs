728f8d6972eab7faaeb43a2c82879cfdb9e3cf98
a-korkin <a-korkin88@yandex.ru>
2025-03-08 15:01:39 +0300

    test: trying async test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,11 +2,20 @@ use http::core::run;
 use reqwest;
 use tokio;
 
+fn spawn_app() {
+    let _ = tokio::spawn(run("127.0.0.1:8080"));
+}
+
 #[tokio::test]
-async fn test_request() {
-    run("127.0.0.1:8080").await;
-    let _response = reqwest::get("localhost:8080")
+async fn test_request_async() {
+    spawn_app();
+
+    let _response = reqwest::get("http://127.0.0.1:8080")
         .await
-        .expect("failed to send request");
+        .expect("failed to get response")
+        .text()
+        .await
+        .expect("failed to get response");
+
     assert!(true == true);
 }
